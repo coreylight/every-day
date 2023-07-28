20378d378665c0d3cef1cf0ba19f1efa39f50c2d
Corey Light <coreylight@gmail.com>
2023-07-28 09:51:00 -0500

    make updates

--- script.mjs
+++ script.mjs
@@ -1,5 +1,6 @@
 import fs from 'fs'
 import path from 'path'
+import cheerio from 'cheerio'
 
 const OPENAI_API = 'https://api.openai.com/v1/chat/completions'
 
@@ -44,6 +45,47 @@ const getAnimalOfTheDayObj = async () => {
   const match2 = html.match(regex2)
   const animal = match2[1]
   return { img: url, animal }
+}
+
+const getWikiMusician = async () => {
+  const date = new Date()
+  const month = date.toLocaleString('default', { month: 'long' })
+  const day = date.getDate()
+  const fetchUrl = `https://en.wikipedia.org/wiki/Portal:Music/DateOfBirth/${month}_${day}`
+
+  const response = await fetch(fetchUrl)
+  const html = await response.text()
+  const $ = cheerio.load(html)
+
+  const getRandomArtistInfo = (idx) => {
+    const els = $('#bodyContent li a:first-child')
+    const randomIndex = Math.floor(Math.random() * els.length)
+    const tag = els.eq(randomIndex)
+    const randomArtistUrl = tag.attr('href')
+    // get inner content of tag
+    const text = tag.text()
+    const name = text.split(',')[0].trim()
+    const firstPartOfName = name.split(' ')[0]
+    const base = 'https://en.wikipedia.org'
+    const url = `${base}${randomArtistUrl}`
+    const doesMatch = url.match(firstPartOfName)
+    if (!doesMatch && idx < 5) {
+      return getRandomArtistInfo(idx + 1)
+    } else {
+      return { url, name }
+    }
+  }
+
+  const { url, name } = getRandomArtistInfo(0)
+  let artistInfo = ''
+  if (url) {
+    const res = await fetch(url)
+    const artistInfoBlob = await res.text()
+    const $ = cheerio.load(artistInfoBlob)
+    $('style').remove()
+    artistInfo = $('#bodyContent').text()
+  }
+  return { url, name, artistInfo }
 }
 
 const LAT = 42.1675
@@ -103,6 +145,8 @@ const getWeather = async () => {
 
   const weatherString = `Riverwoods Weather for today: Min temp: ${minTemp}°F. Max temp: ${maxTemp}°F. Sunrise: ${sunriseTime}. Sunset: ${sunsetTime}.`
 
+  console.log(JSON.stringify({ data }, null, ' '))
+
   return weatherString
 }
 
@@ -116,16 +160,21 @@ const getUnsplashImg = async (query) => {
   return data.urls.small
 }
 
-const htmlPrompt = ({ mucician, weather, animal, animalImg }) => `
+const htmlPrompt = ({
+  musician: { url: musicianUrl, name: musician, text: musicianText },
+  weather,
+  animal,
+  animalImg,
+}) => `
 Give me a "fun facts of the day" html only page that would be fun and useful to elementary students. Javascript is not allowed.
 Style the page in a way that is visually appealing to elementary students using an inline style tag.
 The title of the page should include the current date in humanized and local timezone format according to the browser. The current date in ISO format is: ${new Date().toISOString()}.
 Also include the day of the week in current timezone.
-After the title, include the weather for the area. The current weather is ${weather}.
-After the weather, include an animal of the day, which is the ${animal}. The animal should be accompanied with facts like its habitat, diet, and other interesting information. Include an img tag of the animal with the src of ${animalImg}.
+After the title, this exact text: "The current weather is ${weather}".
+After the weather text, include an animal of the day, which is the ${animal}. The animal should be accompanied with facts like its habitat, diet, and other interesting information. Include an img tag of the animal with the src of ${animalImg}.
 After the animal, the page should contain a color of the day that has a name to it, along with the hex code. Ensure that the color of the day is visually represented so that it uses the color as a background, and chooses white or black text, whichever is better for readability.
-Also include information about the music artist ${mucician}. The info should include their birth year, musical genres, birthplace, and other interesting information.
-For the music artist, include an img tag 400px by 400px horizontally centered with the id attribute "musician", the src attribute of "MUSICIAN-IMG-REPLACE", and the alt attribute of the name of the artist. Also include a link with the musician that is <a href="#" id="link-spotify">Listen to ${mucician} on Spotify 🎶</a>.
+Also include a musician of the day, which is ${musician}. Just provide the name of the musician as a header and an html element that is <p id="musician"/> which will be replaced later.
+For the music artist, include an img tag 400px by 400px horizontally centered with the id attribute "musician-img". Also include a link with the musician that is <a href="#" id="link-spotify" target="_blank">Listen to ${musician} on Spotify 🎶</a>.
 Also include a word of the day that is a good spelling word for 2nd graders. Make the word of the day large in the html. Next, include a dictionary definition of the word. Next, include a sentence appropriate for a 2nd grader that is displayed on its own with the word of the day in it.
 At the end of the page, include a link to the previously generated page. It will be at "archive/DATE.html" (relative path) where the date is the previous day to ${new Date().toISOString()} in YYYY-MM-DD format. Also include a link of the same logic that goes to the next day, which would be the day after ${new Date().toISOString()}.
 For the html code, please ensure your response includes the code within markdown code formatting blocks.
@@ -134,6 +183,7 @@ Include 40px of padding for the html element.
 
 const generateContent = async (prompt, opts) => {
   // replace 'prompt' with your desired prompt
+  console.log({ prompt })
   const res = await fetch(OPENAI_API, {
     method: 'POST',
     headers: openAIHeaders,
@@ -183,6 +233,23 @@ const getAnimalPrompt = () => {
 }
 
 const run = async () => {
+  const {
+    name: musicianName,
+    url: musicianUrl,
+    artistInfo: musicianInfo,
+  } = await getWikiMusician()
+
+  const musicianInfoSummary = await generateContent(`
+  Summarize the information in 2 paragraphs or less.
+  ${musicianInfo}
+`)
+
+  let artistJsonContent = {
+    text: musicianInfoSummary,
+    name: musicianName,
+    url: musicianUrl,
+  }
+
   const animalObj = await getAnimalOfTheDayObj()
   const weather = await getWeather()
   const spotifyToken = await getSpotifyToken()
@@ -200,54 +267,35 @@ const run = async () => {
 
   fs.writeFileSync(archivePath, oldIndexContent)
 
-  const artistContent = await generateContent(`
-    Pick a random music artist in which their birth date is on or close to the current date which is ${new Date().toISOString()}, from any era, dead or alive. Output in json format and limit all prose. An example json structure looks like this:
-    {
-      "name": "Stevie Wonder",
-      "genres": ["pop", "soul", "r&b"],
-      "birthplace": "Saginaw, Michigan",
-      "birthdate": "1950-05-13",
-      "facts": "This multi-talented musician, composer, and singer was born six weeks premature, which resulted in Retinopathy of Prematurity, a condition that led to his blindness. Despite this, Wonder was undeterred and began his career as a child prodigy, signing with Motown's Tamla label at just 11 years old. Known for his captivating voice and extraordinary songwriting abilities, Wonder is the recipient of multiple Grammy Awards, with an impressive total of 25 as of my knowledge cutoff in 2021. He was also the youngest-ever recipient of the Album of the Year Grammy, which he won when he was just 23. One of his many notable achievements includes his induction into the Rock and Roll Hall of Fame in 1989. Furthermore, Stevie is also a civil rights activist and was instrumental in making Martin Luther King Jr.'s birthday a national holiday in the United States. Not just limited to his musical talent, Wonder is also known for playing various instruments, but he is best recognized for his skill on the piano and harmonica."
-    }
-    Now, a new music artist is:
-  `)
-  let artistJsonContent
-  try {
-    artistJsonContent = JSON.parse(artistContent)
-  } catch (err) {
-    //
-  }
-
   const artistName = artistJsonContent?.name || ''
-  if (!artistName) {
-    console.log('No artist generated.')
-    console.log(JSON.stringify(artistJsonContent, null, ' '))
-  }
 
   let htmlContent = await generateContent(
     htmlPrompt({
-      mucician: artistJsonContent?.name,
+      musician: artistJsonContent,
       weather,
       animal: animalObj?.name,
       animalImg: animalObj?.img,
     })
   )
-  const artistInfo = await fetchArtistInfo(spotifyToken, artistName)
-  const firstArtist = artistInfo?.artists?.items?.[0]
-  const theFirstArtistImg = firstArtist?.images?.[0]?.url
-  htmlContent = htmlContent.replace('MUSICIAN-IMG-REPLACE', theFirstArtistImg)
-  htmlContent = htmlContent.replace(
-    'href="#" id="link-spotify"',
-    `href="${firstArtist?.external_urls?.spotify}" id="link-spotify" target="_blank"`
-  )
+
   try {
     htmlContent = htmlContent.split('```html')[1].split('```')[0]
   } catch (err) {
     //
   }
 
+  const $ = cheerio.load(htmlContent)
+  $('#musician').replaceWith(`<p id="musician">${artistJsonContent?.text}</p>`)
+
+  const artistInfo = await fetchArtistInfo(spotifyToken, artistName)
+  const firstArtist = artistInfo?.artists?.items?.[0]
+  const theFirstArtistImg = firstArtist?.images?.[0]?.url
+
+  $('#musician-img').attr('src', theFirstArtistImg).attr('alt', artistName)
+  $('#link-spotify').attr('href', firstArtist?.external_urls?.spotify)
+
   const newIndexContent = `
-    ${htmlContent}
+    ${$.html()}
   `
 
   fs.writeFileSync(oldIndexPath, newIndexContent)
